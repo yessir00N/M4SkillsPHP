6cc36393ec15bd1cc613f6ea2f9964f5ea53fa64
Yessin <34749@ma-web.nl>
2023-06-29 09:16:27 +0200

    Hallo Php

--- structuur/private/Deel2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$elementen = ["voorbeeld 1", "voorbeeld 2", "voorbeeld 3","voorbeeld 4","voorbeeld 5"];
-$elementen2 = [1,2,3,4,5];
-
-for ( $count = 0; $count < count($elementen); $count++) {
-    echo $elementen[$count];
-}
-?>
